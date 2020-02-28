0060afb5d0ef72836aee7f6d7a64e5c9d6c0fe22
Joseph Cross <>
2020-02-27 22:00:12 -0500

    initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,191 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <main>
+        <h1>The current value of Mike's extra $100 per paycheck is</h1>
+        <Loading v-if="loading"/>
+        <h2 v-else>${{ currentValue }}</h2>
+        <div class="select-wrap">
+            <span>Investment:</span>
+            <select v-model="symbol" @change="calculate">
+                <option value="SPY">S&amp;P 500 ETF</option>
+                <option value="AMD">AMD</option>
+                <option value="AAPL">Apple</option>
+                <option value="BYND">Beyond Meat</option>
+                <option value="DIS">Disney</option>
+                <option value="FB">Facebook</option>
+                <option value="GE">GE</option>
+                <option value="MSFT">Microsoft</option>
+                <option value="SBUX">Starbucks</option>
+                <option value="TSLA">Tesla</option>
+                <option value="UBER">Uber</option>
+            </select>
+        </div>
+    </main>
 </template>
 
 <script>
+import _ from 'lodash';
+import axios from 'axios';
+import Loading from './Loading';
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  components: {
+    Loading,
+  },
+  data() {
+    return {
+      currentPrice: null,
+      loading: false,
+      symbol: 'SPY',
+      apiKey: 'bpc69o7rh5rfp0uqdgsg',
+      dates: [
+        '1/15/2020',
+        '2/01/2020',
+        '2/15/2020',
+        '3/01/2020',
+        '3/15/2020',
+        '4/01/2020',
+        '4/15/2020',
+        '5/01/2020',
+        '5/15/2020',
+        '6/01/2020',
+        '6/15/2020',
+        '7/01/2020',
+        '7/15/2020',
+        '8/01/2020',
+        '8/15/2020',
+        '9/01/2020',
+        '9/15/2020',
+      ],
+      pastPrices: [],
+    };
+  },
+  created() {
+    this.calculate();
+  },
+  methods: {
+    calculate() {
+      this.loading = true;
+      this.pastPrices = [];
+      this.getCurrentPrice(this.symbol);
+      this.getPastPrices(this.symbol);
+    },
+    getCurrentPrice(symbol) {
+      const url = `https://finnhub.io/api/v1/quote/?symbol=${symbol}&token=${this.apiKey}`;
+
+      axios.get(url)
+        .then((response) => {
+          this.currentPrice = response.data.c;
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+    getPastPrices(symbol) {
+      const promises = [];
+      const today = new Date().getTime();
+
+      _.forEach( this.dates, (date) => {
+        const payday = new Date(date).getTime();
+        if ( today >= payday ) {
+          promises.push(this.getPrice(symbol, date));
+        }
+      });
+
+      Promise.all(promises).then( () => {
+        this.loading = false;
+      });
+    },
+    getPrice(symbol, date) {
+      const day = new Date(date);
+      let timeStart = Math.round(day.getTime()/1000);
+
+      if ( day.getDay() === 6 ) {
+        timeStart += 172800;
+      }
+
+      if ( day.getDay() === 0 ) {
+        timeStart += 86400;
+      }
+
+      const timeEnd = timeStart + 86400;
+      const url = `https://finnhub.io/api/v1/stock/candle?symbol=${symbol}&resolution=60&from=${timeStart}&to=${timeEnd}&token=${this.apiKey}`;
+      return axios.get(url)
+        .then( (response) => {
+          if ( response.data.c ) {
+            console.log('past price: ', response.data.c[0]);
+            this.pastPrices.push( response.data.c[0] );
+          } else {
+            console.log('!!!! no price data for this day', date);
+          }
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+  },
+  computed: {
+    currentValue() {
+      const lots = [];
+
+      if ( !this.pastPrices.length ) {
+        return 0;
+      }
+
+      _.forEach( this.pastPrices, (price) => {
+        const percentGain = ( this.currentPrice - price ) / price;
+        lots.push( 100 + (100 * percentGain) );
+      });
+
+      const total = lots.reduce((a, b) => a + b, 0);
+      return Math.round(total * 100) / 100
+    },
+  },
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
+<style>
+
+h1 {
+    font-size: 28px;
+    margin: 0;
+    line-height: 1.4;
+}
+
+h2 {
+  font-size: 80px;
+  line-height: 1.2;
+  color: #28881c;
   margin: 40px 0 0;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+@media screen and ( max-width: 768px ) {
+    h1 {
+        font-size: 21px;
+    }
+    h2 {
+        font-size: 60px;
+        margin-top: 20px;
+    }
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+main {
+    padding-left: 1rem;
+    padding-right: 1rem;
+    height: 100vh;
+    max-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 20vh;
+    padding-bottom: 32px;
 }
-a {
-  color: #42b983;
+
+.select-wrap {
+    margin-top: auto;
+}
+.select-wrap span {
+    margin-right: 10px;
 }
 </style>
---
+++ src/components/Loading.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="loading">
+        <div class="bounce1"></div>
+        <div class="bounce2"></div>
+        <div class="bounce3"></div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Loading',
+};
+</script>
+
+<style lang="scss">
+    .loading {
+        margin-top: 40px;
+        padding: 2rem;
+        height: 96px;
+        display: flex;
+        align-items: center;
+        width: auto;
+        text-align: center;
+
+        div {
+            width: 20px;
+            height: 20px;
+            background-color: #2c3e50;
+            margin: 0 8px;
+
+            border-radius: 100%;
+            display: inline-block;
+            -webkit-animation: sk-bouncedelay 1.4s infinite ease-in-out both;
+            animation: sk-bouncedelay 1.4s infinite ease-in-out both;
+        }
+    }
+
+    @media screen and ( max-width: 768px ) {
+        .loading {
+            height: 72px;
+            margin-top: 20px;
+
+            div {
+                width: 16px;
+                height: 16px;
+                margin: 0 7px;
+            }
+        }
+    }
+
+    .loading .bounce1 {
+      -webkit-animation-delay: -0.32s;
+      animation-delay: -0.32s;
+    }
+
+    .loading .bounce2 {
+      -webkit-animation-delay: -0.16s;
+      animation-delay: -0.16s;
+    }
+
+    @-webkit-keyframes sk-bouncedelay {
+      0%, 80%, 100% { -webkit-transform: scale(0) }
+      40% { -webkit-transform: scale(1.0) }
+    }
+
+    @keyframes sk-bouncedelay {
+      0%, 80%, 100% {
+        -webkit-transform: scale(0);
+        transform: scale(0);
+      } 40% {
+        -webkit-transform: scale(1.0);
+        transform: scale(1.0);
+      }
+    }
+
+</style>
